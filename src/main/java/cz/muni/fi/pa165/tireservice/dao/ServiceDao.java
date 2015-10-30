ccc32053cfec61accad19e6e87781b1aecd0ad5c
Michal Kysilko <michal@ubuntu.(none)>
2015-10-30 17:40:15 +0100

    added ServiceDao.java, modified Service.java

---
+++ src/main/java/cz/muni/fi/pa165/tireservice/dao/ServiceDao.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pa165.tireservice.dao;
+
+/**
+ *
+ * @author Michal Kysilko
+ */
+public class ServiceDao {
+    
+}
--- src/main/java/cz/muni/fi/pa165/tireservice/entity/Service.java
+++ src/main/java/cz/muni/fi/pa165/tireservice/entity/Service.java
@@ -1,14 +1,94 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.tireservice.entity;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 /**
  *
- * @author 
+ * @author Michal Kysilko
  */
+
+@Entity
+@Table(name = "SERVICE")
 public class Service {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+    
+    @NotNull
+    private BigDecimal price;
+    
+    
+    public Service() {
+    }
+
+    public Service(Long id) {
+        this.id = id;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public BigDecimal getPrice() {
+        return price;
+    }
+    
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.getName());
+        hash = 37 * hash + Objects.hashCode(this.getPrice());
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        
+        if (obj == null) {
+            return false;
+        }
+        
+        if (!(obj instanceof Service)) {
+            return false;
+        }
+        
+        final Service other = (Service) obj;
+        
+        if (!Objects.equals(this.name, other.getName())) {
+            return false;
+        }
+        
+        if (!Objects.equals(this.price, other.getPrice())) {
+            return false;
+        }
+        
+        return true;
+    }
     
 }
