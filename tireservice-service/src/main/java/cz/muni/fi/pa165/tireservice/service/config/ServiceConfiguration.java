896c21bbc5ffab0b1f23d149d3bb58a8403e9441
Jan Tymel <jtymel@redhat.com>
2015-11-21 16:40:42 +0100

    Project structure updated according to milestone 2 requirements

---
+++ tireservice-service/src/main/java/cz/muni/fi/pa165/tireservice/service/config/ServiceConfiguration.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.pa165.tireservice.service.config;
+
+import cz.muni.fi.pa165.PersistenceSampleApplicationContext;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+
+@Configuration
+@Import(PersistenceSampleApplicationContext.class)
+//@ComponentScan(basePackageClasses={OrderServiceImpl.class, CategoryFacadeImpl.class})
+public class ServiceConfiguration {
+
+
+	@Bean
+	public Mapper dozer(){
+		DozerBeanMapper dozer = new DozerBeanMapper();
+		dozer.addMapping(new DozerCustomConfig());
+		return dozer;
+	}
+
+	public class DozerCustomConfig extends BeanMappingBuilder {
+	    @Override
+	    protected void configure() {
+//	        mapping(Category.class, CategoryDTO.class);
+	    }
+	}
+
+}
+
