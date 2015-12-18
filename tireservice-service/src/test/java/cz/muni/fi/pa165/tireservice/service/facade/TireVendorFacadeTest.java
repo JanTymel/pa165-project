58c85271f2edb36dfb8907dc1c5fde22b6eb9070
kysna <michalkysilko@gmail.com>
2015-12-18 17:44:26 +0100

    renamed, changed junit to testNg testing

--- tireservice-service/src/test/java/cz/muni/fi/pa165/tireservice/service/facade/TireVendorFacadeTest.java
+++ tireservice-service/src/test/java/cz/muni/fi/pa165/tireservice/service/facade/TireVendorFacadeTest.java
@@ -3,32 +3,29 @@ package cz.muni.fi.pa165.tireservice.service.facade;
 import cz.muni.fi.pa165.tireservice.dto.TireVendorDto;
 import cz.muni.fi.pa165.tireservice.entity.TireVendor;
 import cz.muni.fi.pa165.tireservice.facade.TireVendorFacade;
+import cz.muni.fi.pa165.tireservice.service.config.ServiceConfiguration;
 import cz.muni.fi.pa165.tireservice.sevice.BeanMappingService;
 import cz.muni.fi.pa165.tireservice.sevice.TireVendorService;
 import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.service.spi.ServiceException;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import static org.testng.Assert.assertEquals;
+import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 /**
  *
  * @author Jan Tymel
  */
-@TestExecutionListeners(TransactionalTestExecutionListener.class)
-@RunWith(MockitoJUnitRunner.class)
-public class TireVendorFacadeTestCase {
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class TireVendorFacadeTest {
 
     @Mock
     private BeanMappingService beanMappingService;
@@ -78,14 +75,14 @@ public class TireVendorFacadeTestCase {
         vendorDtoList.add(vendorDto2);
     }
 
-//    @Test
+    @Test
     public void testGetAllTireVendors() {
         when(tireVendorService.findAll()).thenReturn(vendorList);
         when(beanMappingService.mapTo(Matchers.anyCollection(), Matchers.eq(TireVendorDto.class))).thenReturn(vendorDtoList);
         assertEquals(tireVendorFacade.getAllTireVendors().size(), 2);
     }
 
-//    @Test
+    @Test
     public void testGetTireVendorById() {
         when(tireVendorService.findById(1l)).thenReturn(vendor1);
         when(beanMappingService.mapTo(vendor1, TireVendorDto.class)).thenReturn(vendorDto1);
