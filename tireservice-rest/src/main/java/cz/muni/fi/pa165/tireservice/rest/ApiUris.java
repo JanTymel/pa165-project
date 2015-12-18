dfef2dc1d2464d8ca24356efe684ad54583067ba
kysna <michalkysilko@gmail.com>
2015-12-18 15:03:10 +0100

    rest, not finished

---
+++ tireservice-rest/src/main/java/cz/muni/fi/pa165/tireservice/rest/ApiUris.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.tireservice.rest;
+
+public abstract class ApiUris {
+    public static final String ROOT_URI_SERVICES   = "/services"; 
+    public static final String ROOT_URI_TIRES      = "/tires";
+    public static final String ROOT_URI_ORDERS     = "/orders";
+    public static final String ROOT_URI_USERS = "/users";  
+}
